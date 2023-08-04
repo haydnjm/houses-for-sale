3363a1aeeb2a953d2a45670b0bc6ea6a99625ab4
Haydn <haydn.morris@ivy-solutions.de>
2023-08-04 08:39:31 +0200

    fix stuff

--- scraper/src/data/write_to_big_query.py
+++ scraper/src/data/write_to_big_query.py
@@ -12,27 +12,27 @@ bq_table = os.getenv("BQ_TABLE")
 def write_to_bigquery(client, rows):
     print(f"Attempting to write {len(rows)} rows to BigQuery")
     # Big query table Id
-    # table_id = f"{gcp_project}.{bq_dataset}.{bq_table}"
+    table_id = f"{gcp_project}.{bq_dataset}.{bq_table}"
 
-    # for row in rows:
-    #     # Check if row already exists in BigQuery
-    #     # If the house was added more than 7 days ago, re-add it
-    #     buffer = datetime.fromisoformat(row["inserted_date"]) - timedelta(days=7)
-    #     query = f'SELECT * FROM `{table_id}` where id="{row["id"]}" and inserted_date > TIMESTAMP("{buffer}") LIMIT 1'
+    for row in rows:
+        # Check if row already exists in BigQuery
+        # If the house was added more than 7 days ago, re-add it
+        buffer = datetime.fromisoformat(row["inserted_date"]) - timedelta(days=7)
+        query = f'SELECT * FROM `{table_id}` where id="{row["id"]}" and inserted_date > TIMESTAMP("{buffer}") LIMIT 1'
 
-    #     results = client.query(query).result()
-    #     results_list = list(results)
+        results = client.query(query).result()
+        results_list = list(results)
 
-    #     if len(results_list) == 0:
-    #         # Insert the rows into BigQuery table
-    #         errors = client.insert_rows_json(
-    #             table_id,
-    #             [row],
-    #         )
+        if len(results_list) == 0:
+            # Insert the rows into BigQuery table
+            errors = client.insert_rows_json(
+                table_id,
+                [row],
+            )
 
-    #         if errors:
-    #             print(f"Encountered errors while inserting {row.id}: {errors}")
-    #         else:
-    #             print(f"Successfully inserted {row['id']}")
-    #     else:
-    #         print(f"Row with id {row['id']} already exists in BigQuery.")
+            if errors:
+                print(f"Encountered errors while inserting {row.id}: {errors}")
+            else:
+                print(f"Successfully inserted {row['id']}")
+        else:
+            print(f"Row with id {row['id']} already exists in BigQuery.")
